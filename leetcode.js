0c3d3f2deaa34c01d5fae6ad15d9bc912a9887b6
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-02-09 10:49:01 -0800

    add solution to lc 151

--- leetcode.js
+++ leetcode.js
@@ -162,3 +162,29 @@ var reverseVowels = function(s) {
 
   return result.join('')
 };
+
+// 151 . Reverse Words in a String - easy Feb. 9, 2025
+
+//Given an input string s, reverse the order of the words.
+
+// A word is defined as a sequence of non-space characters. 
+// The words in s will be separated by at least one space. 
+// Return a string of the words in reverse order concatenated by a single space.
+// Note that s may contain leading or trailing spaces or multiple spaces between two words. 
+// The returned string should only have a single space separating the words. 
+// Do not include any extra spaces.
+
+var reverseWords = function(s) {
+  let arr = s.split(' ');
+  let result = [];
+
+  for ( let i = arr.length-1 ; i >= 0 ; i-- ) {
+      if (arr[i] === '') {
+          continue
+      } else {      
+          result.push(arr[i])         
+      }
+  }
+
+  return result.join(' ')
+};
