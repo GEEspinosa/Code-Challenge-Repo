dcbd2bea825d4c9ef8d1d0286e317acce342af4e
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-03-13 11:44:46 -0700

    adding solution to lc 27

--- leetcode.js
+++ leetcode.js
@@ -587,3 +587,30 @@ var isPalindrome = function(x) {
   return norm === pal
   
 };
+
+
+// 27. Remove Elements - easy Mar 13, 2025
+
+// Given an integer array nums and an integer val, 
+// remove all occurrences of val in nums in-place. 
+// The order of the elements may be changed. 
+// Then return the number of elements in nums which are not equal to val.
+// Consider the number of elements in nums which are not equal to val be k, 
+// to get accepted, you need to do the following things:
+// Change the array nums such that the first k elements of nums 
+// contain the elements which are not equal to val. 
+// The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+//Dev Note: iterate backwards to reduce time complexity of splice method.
+
+var removeElement = function(nums, val) {
+
+  for ( let i =  nums.length - 1 ; i >= 0 ; i-- ) {
+      if (nums[i] === val) {
+          nums.splice(i, 1)
+      } 
+  }
+  return nums.length
+  
+};
