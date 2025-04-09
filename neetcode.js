f7e830cd898303404f602626069ef26cb692bd87
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-04-08 17:57:42 -0700

    adding nc Two Sum problem solution

--- neetcode.js
+++ neetcode.js
@@ -21,10 +21,10 @@ function hasDuplicate(nums) {
 
 //Valid Anagram - easy Apr 8, 2025
 
-//Given two strings s and t, 
-// return true if the two strings are anagrams of each other, 
-// otherwise return false. An anagram is a string that contains 
-// the exact same characters as another string, 
+//Given two strings s and t,
+// return true if the two strings are anagrams of each other,
+// otherwise return false. An anagram is a string that contains
+// the exact same characters as another string,
 // but the order of the characters can be different.
 
 //dev note: build up a hashtable then delete properties on second string iteration
@@ -55,4 +55,34 @@ function isAnagram(s, t) {
   }
 
   return Object.keys(hashObj).length === 0;
+}
+
+//Two Sum - easy Apr 8, 2025
+
+//Given an array of integers nums and an integer target, 
+// return the indices i and j such that nums[i] + nums[j] == target and i != j.
+// You may assume that every input has exactly one 
+// pair of indices i and j that satisfy the condition.
+// Return the answer with the smaller index first.
+
+function twoSum(nums, target) {
+  let sorted = [];
+  let beg = 0;
+  let end = nums.length - 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    sorted.push([nums[i], i]);
+  }
+
+  sorted.sort((a, b) => a[0] - b[0]);
+
+  while (beg < end) {
+    if (sorted[beg][0] + sorted[end][0] === target) {
+      return [sorted[beg][1], sorted[end][1]];
+    } else if (sorted[beg][0] + sorted[end][0] > target) {
+      end--;
+    } else if (sorted[beg][0] + sorted[end][0] < target) {
+      beg++;
+    }
+  }
 }
