2117ce17e9b9efe29e9c185aab2d7bfa423be2a9
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-05-31 11:25:33 -0700

    messing with card/deck building OOP logic using js class and functions

--- misc practice.js
+++ misc practice.js
@@ -22,4 +22,32 @@ function shapeFit(shape, height, width) {
   return true;
 }
 
-console.log(shapeFit(square1, 5, 3));
+//console.log(shapeFit(square1, 5, 3));
+
+let SUITS = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
+let VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+
+class Card {
+  constructor (suit, value){
+    this.suit = suit
+    this.value = value
+  }
+
+  static random(){
+    const suit = SUITS[Math.floor(Math.random() * SUITS.length)]
+    const value = VALUES[Math.floor(Math.random() * VALUES.length)]
+    return new Card(suit, value)
+  }
+}
+
+function buildDeck () {
+  let deck = [];
+  for (let suit of SUITS) {
+    for (let value of VALUES){
+      deck.push(new Card(suit, value))
+    }
+  }
+  return deck
+}
+
+console.log(buildDeck())
